eb802e8a1c0c211d01a962760a2647e2a6807445
kkazlou <59448734+kkazlou@users.noreply.github.com>
2024-06-19 10:51:34 -0500

    added PUT and DELETE methods

--- app.py
+++ app.py
@@ -22,11 +22,41 @@ def get_store(store_id):
 def create_store():
     store_data = request.get_json()
 
+    if "name" not in store_data:
+        return {"message": "Bad request. Ensure 'name', is inclued in the JSON payload."}, 400
+    
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            return {"message": "Store already exists."}, 400
+
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
 
-    return store, 201 
+    return store, 201
+
+@app.put("/store/<string:store_id>")
+def update_store(store_id):
+    store_data = request.get_json()
+
+    if "name" not in store_data:
+        return {"message": "Bad request. Ensure 'name', is inclued in the JSON payload."}, 400
+
+    if store_id in stores:
+        store = {**store_data, "id": store_id}
+        stores[store_id] = store
+        return store
+    else:
+        return {"message": "Store not found"}, 404
+
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted"}
+    except KeyError:
+        return {"message": "Store not found"}, 404
 
 
 @app.get("/item")
@@ -45,12 +75,20 @@ def get_item(item_id):
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
+
     if (
         "price" not in item_data
         or "store_id" not in item_data
         or "name" not in item_data
     ):
-        return {"message": "Bad request. Ensure 'price', 'store_id' and 'name' are inclued in the JSON payload."}
+        return {"message": "Bad request. Ensure 'price', 'store_id' and 'name' are inclued in the JSON payload."}, 400
+    
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            return {"message": "Item already exists."}, 400
 
     if item_data["store_id"] not in stores:
         return {"message": "Store not found"}, 404
@@ -60,3 +98,29 @@ def create_item():
     items[item_id] = item
 
     return item, 201
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+
+    if (
+        "price" not in item_data
+        or "name" not in item_data
+    ):
+        return {"message": "Bad request. Ensure 'price' and 'name' are inclued in the JSON payload."}, 400
+
+    if item_id in items:
+        item = {**item_data, "id": item_id}
+        items[item_id] = item
+        return item
+    else:
+        return {"message": "Item not found"}, 404
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted"}
+    except KeyError:
+        return {"message": "Item not found"}, 404
